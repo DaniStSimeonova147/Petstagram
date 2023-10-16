c3dec09e507cf522718ed37de97fd06e74c1fd85
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-16 17:15:21 +0300

    Create photo model

---
+++ src/models/Photo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const photoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required!'],
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required!'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required!'],
+    },
+    // commentList: {
+    //     type: [],
+    // },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Photo = mongoose.model('Photo', userSchema);
+
+module.exports = Photo;
